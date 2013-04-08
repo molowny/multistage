21eb242772ba6ad066023699ffa1e3e75c132302
Mariusz Ołownia <ollownia@gmail.com>
2013-04-08 14:26:19 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Multistage::Application.config.secret_token = '06d4756934fdf6212f840838c03681ca0218afcebdcdcd63e2bde38f137aeb3c0e015af66ca52555c52da4dea5ac51672f3c29aa41ec321895ac7481bac842ba'
